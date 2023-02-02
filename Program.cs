53ff0866485dff65e6b255c070786eb95d97e632
mutableGrape <danielworton@live.co.uk>
2023-02-02 10:09:26 +0000

    Warm-up 1

--- Program.cs
+++ Program.cs
@@ -10,6 +10,27 @@ namespace Problems2
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("CodingBat Exercies, one chosen at random from each section");
+            Console.WriteLine();
+
+            Console.WriteLine("Warmup-1: max1020");
+            Console.WriteLine(max1020(11, 19));
+            Console.ReadLine();
+
+
+        }
+
+        static int max1020(int a, int b)
+        {
+            int c = 0;
+            int d = 0;
+            if (a > b) { c = a; d = b; }
+            else { c = b; d = a; }
+
+            if ((10 <= c) && (c <= 20)) { return c; }
+            else if ((10 <= d) && (d <= 30)) { return d; }
+            else { return 0; }
+
         }
     }
 }
